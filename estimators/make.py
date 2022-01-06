f0cb2559370f6308d6fbe0cd08ba9b0923987990
Greg Coppola <greg@hiro.so>
2022-01-06 21:52:32 +0000

    use lines and limits

--- estimators/make.py
+++ estimators/make.py
@@ -38,27 +38,25 @@ def extract_list(condition, heading, display):
     x = []
     for kv, idx in zip(kv_list, idx_list):
         base = kv[heading] * COST_FACTOR
-        if display == 'natural':
+        if display == 'original' or display == 'cutoff':
             y.append(base)
             x.append(idx)
         if display == 'log':
             y.append(math.log(base, 10.0))
             x.append(idx)
-        if display == 'cutoff':
-            if base < 10.0:
-                y.append(base)
-                x.append(idx)
     return [y, x]
 
 levels = ['high', 'middle', 'low']
-displays = ['natural', 'log', 'cutoff']
+displays = ['original', 'log']
 for  level in levels:
     for display in displays:
         median = extract_list('median', 'new_estimate_' + level, display)
         old = extract_list('old', 'new_estimate_' + level, display)
 
         plt.figure(figsize=(20, 10))
+        f, ax = plt.subplots(1, 1)
         plt.clf()
-        plt.scatter(median[1], median[0])
-        plt.scatter(old[1], old[0])
+        ax.set_ylim(0.0, 10.0)
+        ax.plot(median[1], median[0])
+        ax.plot(old[1], old[0])
         plt.savefig(level + '_' + str(display) + '.svg')
