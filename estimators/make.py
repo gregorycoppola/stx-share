521efc8a535bc60cc1106f7d91d1b1aff1df0df9
Greg Coppola <greg@hiro.so>
2022-01-06 22:04:34 +0000

    should have labels

--- estimators/make.py
+++ estimators/make.py
@@ -55,9 +55,14 @@ for  level in levels:
 
         plt.figure(figsize=(20, 10))
         plt.clf()
+        plt.title('New vs. Old Fee Estimator')
+        plt.xlabel('Block Height')
+        plt.ylabel('Estimated fee for "stack-stx"')
         f, ax = plt.subplots(1, 1)
-        plt.plot(old[1], old[0])
-        plt.plot(median[1], median[0])
+        line1, = plt.plot(old[1], old[0])
+        line1.set_label('old')
+        line2, = plt.plot(median[1], median[0])
+        line2.set_label('new')
         if display == 'cutoff':
-            ax.set_ylim(0.0, 12.0)
+            ax.set_ylim(0.0, 10.0)
         plt.savefig(level + '_' + str(display) + '.svg')
