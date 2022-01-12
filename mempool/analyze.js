5cfdda9a00875a1894c55760eeca12ba1f9c46cf
Greg Coppola <greg@hiro.so>
2022-01-12 18:31:38 +0000

    add the map test

--- mempool/analyze.js
+++ mempool/analyze.js
@@ -1,18 +1,35 @@
 const axios = require('axios')
 
+function add_to_map(base_dict, key) {
+    if key in base_dict {
+        base_dict[key] += 1 
+    }
+}
+
 async function main() {
-    const result = await axios
+    const mempool_result = await axios
         .post('https://stacks-node-api.mainnet.stacks.co/rosetta/v1/mempool', {
             network_identifier: {
                 blockchain: "stacks",
                 network: "mainnet"
             }
         })
-    let data = result.data
 
-    for (const tx of result.data.transaction_identifiers) {
-        let hash = tx.hash
-        console.log({hash})
+    let data = mempool_result.data
+    for (const tx of mempool_result.data.transaction_identifiers) {
+        try {
+            const tx_result = await axios
+                .get('https://stacks-node-api.mainnet.stacks.co/extended/v1/tx/' + tx.hash)
+            let data = tx_result.data
+            let tx_status = data.tx_status
+            console.log({
+                tx_status
+            })
+        } catch (e) {
+            console.log({
+                e
+            })
+        }
     }
 }
 
---
+++ mempool/map_test.js
@@ -0,0 +1,23 @@
+const map1 = new Map();
+map1.set('bar', 'foo');
+
+
+console.log({
+    map1
+})
+
+
+
+{
+    const val = map1.get('bar');
+    console.log({
+        val
+    })
+}
+
+{
+    const val = map1.get('bar2');
+    console.log({
+        val
+    })
+}
