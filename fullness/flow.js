573860119750066cc313f7032d2a3c127796c956
Greg Coppola <greg@hiro.so>
2022-01-17 19:25:12 +0000

    fixed flow script by removing the "status=1" part

--- fullness/flow.js
+++ fullness/flow.js
@@ -9,7 +9,7 @@ async function main() {
     const client = new Client()
     await client.connect()
     // const res = await client.query('SELECT NOW()')
-    const res = await client.query('select block_hash, block_height, tx_id, status, microblock_hash, execution_cost_read_count, execution_cost_read_length, execution_cost_runtime, execution_cost_write_count, execution_cost_write_length, length(raw_result) from txs where status = 1 order by block_height desc limit 5000')
+    const res = await client.query('select block_hash, block_height, tx_id, status, microblock_hash, execution_cost_read_count, execution_cost_read_length, execution_cost_runtime, execution_cost_write_count, execution_cost_write_length, length(raw_result) from txs order by block_height desc limit 5000')
 
     const block_hash_set = new Set()
     const block_txs_map = new Map()
