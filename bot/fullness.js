9d60c03da309fc518fe4680f0c96bf1f4f20c238
Greg Coppola <greg@hiro.so>
2022-01-20 05:25:26 +0000

    clean up logging; running

--- bot/fullness.js
+++ bot/fullness.js
@@ -146,26 +146,22 @@ async function write_tuples_to_db(output_tuples, output_client) {
 }
 
 async function run() {
+    console.log('fullness: starting')
     const input_config = read_config(process.argv[2], 'input_config')
     const output_config = read_config(process.argv[3], 'output_config')
-    console.log({
-        input_config,
-        output_config,
-    })
     const input_client = new Client(input_config)
     await input_client.connect()
     const output_client = new Client(output_config)
     await output_client.connect()
     try {
         const previous_max_block_height = await get_previous_max_block_height(output_client)
-        console.log({
-            previous_max_block_height
-        })
         const select_output = await select_new_blocks(input_client, previous_max_block_height)
         const output_tuples = create_output_tuples(select_output)
         write_tuples_to_db(output_tuples, output_client)
+        console.log('fullness: success')
 
     } catch (error) {
+        console.log('fullness: error')
         console.log({
             error
         })
--- bot/main.js
+++ bot/main.js
@@ -2,21 +2,24 @@ const process = require('process')
 const fullness = require('./fullness')
 
 function sleep(ms) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
 }
 
 async function main() {
-    while(true) {
+    while (true) {
+        let date_ob = new Date();
         const sleep_time = 60 * 1000
-        console.log(`sleeping... ${sleep_time}`)
+        console.log(date_ob.toString() + `: sleeping for ${sleep_time}ms`)
         await sleep(sleep_time)
 
         try {
             await fullness.run()
         } catch (e) {
-            console.log({e})
+            console.log({
+                e
+            })
         }
     }
 }
