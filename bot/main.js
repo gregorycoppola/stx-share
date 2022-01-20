ea00fab76aaa61af7adea45767ff48aafd79eee9
Greg Coppola <greg@hiro.so>
2022-01-20 00:04:41 +0000

    added sleep

--- bot/main.js
+++ bot/main.js
@@ -1,11 +1,16 @@
-const {
-    Pool,
-    Client
-} = require('pg')
-
 const process = require('process')
 
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
 async function main() {
+    while(true) {
+        console.log("sleeping...")
+        await sleep(1000)
+    }
 }
 
 main()
