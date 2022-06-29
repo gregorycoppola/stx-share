052c371f750074fe224fd199e580212a776803f4
Greg Coppola <greg@hiro.so>
2022-06-29 15:12:15 -0500

    make it a server

--- mempool/all_mempool.js
+++ mempool/all_mempool.js
@@ -6,35 +6,25 @@ function add_to_map(count_map, key) {
     count_map.set(key, count + 1)
 }
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 async function main() {
-	console.log('hello')
-
-    const mempool_result = await axios
-        .post('http://127.0.0.1:3999/rosetta/v1/mempool', {
-            network_identifier: {
-                blockchain: "stacks",
-                network: "mainnet"
-            }
-        })
-	console.log({mempool_result})
-
-    const count_map = new Map()
-    for (const tx of mempool_result.data.transaction_identifiers) {
-        try {
-            const tx_result = await axios
-                .get('http://127.0.0.1:3999/extended/v1/tx/' + tx.hash)
-            let data = tx_result.data
-            let tx_status = data.tx_status
-            console.log(data)
-        } catch (e) {
-            console.log({
-                e
+    while (true) {
+        const mempool_result = await axios
+            .post('https://stacks-node-api.mainnet.stacks.co/rosetta/v1/mempool', {
+                network_identifier: {
+                    blockchain: "stacks",
+                    network: "mainnet"
+                }
             })
-        }
+
+        const date = new Date()
+        const ids = mempool_result.data.transaction_identifiers
+        console.log(date, {id_size : ids.length})
+        await sleep(30 * 1000)
     }
-    console.log({
-        count_map
-    })
 }
 
 main()
