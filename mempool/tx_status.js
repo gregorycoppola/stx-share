479400169b4d8251c4ec564c6cc4b0ea8c7c12b6
Greg Coppola <greg@hiro.so>
2022-06-30 09:39:26 -0500

    make tx_status use the api

--- mempool/tx_status.js
+++ mempool/tx_status.js
@@ -1,8 +1,9 @@
 const axios = require('axios')
 
+const base_url = 'https://stacks-node-api.mainnet.stacks.co/'
 async function main() {
     const mempool_result = await axios
-        .post('http://127.0.0.1:3999/rosetta/v1/mempool', {
+        .post(base_url + 'rosetta/v1/mempool', {
             network_identifier: {
                 blockchain: "stacks",
                 network: "mainnet"
@@ -13,7 +14,7 @@ async function main() {
     for (const tx of mempool_result.data.transaction_identifiers) {
         try {
             const tx_result = await axios
-                .get('http://127.0.0.1:3999/extended/v1/tx/' + tx.hash)
+                .get(base_url + 'extended/v1/tx/' + tx.hash)
             let data = tx_result.data
             let tx_status = data.tx_status
             console.log({
