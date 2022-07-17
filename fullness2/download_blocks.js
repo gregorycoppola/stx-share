7e4923df1391a8329c57e90cbdabf5a138bd8595
Greg Coppola <greg@hiro.so>
2022-07-17 05:31:18 -0500

    adding json

--- fullness2/download_blocks.js
+++ fullness2/download_blocks.js
@@ -1,11 +1,10 @@
-https://stacks-node-api.mainnet.stacks.co/extended/v1/block
-
 // Step 1:
-// curl curl https://stacks-node-api.mainnet.stacks.co/extended/v1/block > ${TX_JSON_FNAME}
+// curl https://stacks-node-api.mainnet.stacks.co/extended/v1/block > ${FILE_NAME}
+
+// Step 2:
+// node download_blocks.js ${FILE_NAME}
 
 const process = require('process')
 
 TX_JSON_FNAME = process.argv[1]
-
-
 console.log({TX_JSON_FNAME})
